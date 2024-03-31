abe739121e53f85fd4971c7e5dcd11b3fd131ce5
Ghifaryh <mhghifaryy@gmail.com>
2024-03-31 10:15:06 +0700

    finalizing api docs

--- be-test-case/routes/BorrowRoute.ts
+++ be-test-case/routes/BorrowRoute.ts
@@ -1,3 +1,85 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Borrow:
+ *       type: object
+ *       required:
+ *         - id
+ *         - memberCode
+ *         - bookCode
+ *         - borrowedAt
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the borrows
+ *         memberCode:
+ *           type: string
+ *           description: The code (id) of the member
+ *         bookCode:
+ *           type: string
+ *           description: The code (id) of tthe book
+ *         borrowedAt:
+ *           type: string
+ *           format: date
+ *           description: The date book is borrowed from
+ *         returnedAt:
+ *           type: string
+ *           format: date
+ *           description: The date book is returned from
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Borrow
+ *   description: The borrows manager
+ * /borrow:
+ *   get:
+ *     summary: Lists all the borrows
+ *     tags: [Borrow]
+ *     responses:
+ *       200:
+ *         description: The list of the borrows
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Borrow'
+ *   patch:
+ *    summary: Borrow or return a book
+ *    tags: [Borrow]
+ *    requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               action:
+ *                 type: string
+ *                 enum: [borrow, return]
+ *                 description: Action to perform (borrow or return)
+ *               memberCode:
+ *                 type: string
+ *                 description: Member code for borrowing/returning book
+ *               bookCode:
+ *                type: string
+ *                description: Book code for borrowing/returning book
+ *    responses:
+ *      200:
+ *        description: The borrow is updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Borrow'
+ *      404:
+ *        description: The book is not found
+ *      500:
+ *        description: Some error happened
+ */
+
 import express from 'express'
 import { borrowBook, getBorrow, returnBook } from '../controller/BorrowController'
 
@@ -6,7 +88,7 @@ const router = express.Router()
 router.get('/borrow', getBorrow)
 
 router.patch('/borrow', (req, res) => {
-  const action = req.body.action;
+  const action = req.body.action
   if (action == 'borrow') {
     borrowBook(req, res)
   } else if (action == 'return') {
--- be-test-case/routes/MemberRoute.ts
+++ be-test-case/routes/MemberRoute.ts
@@ -1,3 +1,41 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Members:
+ *       type: object
+ *       required:
+ *         - code
+ *         - name
+ *       properties:
+ *         code:
+ *           type: string
+ *           description: The unique identifier of the members
+ *         name:
+ *           type: string
+ *           description: Member name
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Members
+ *   description: The Members manager
+ * /members:
+ *   get:
+ *     summary: Lists all the member
+ *     tags: [Members]
+ *     responses:
+ *       200:
+ *         description: The list of the members
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Members'
+ */
+
 import express from 'express'
 import { getMember } from '../controller/MemberController'
 
