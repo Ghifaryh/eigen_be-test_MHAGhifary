4da116803a8aa43279b4582019e2223e93e867f1
Ghifaryh <mhghifaryy@gmail.com>
2024-03-31 09:42:04 +0700

    add api documentation

--- be-test-case/index.ts
+++ be-test-case/index.ts
@@ -3,6 +3,9 @@ import BookRoute from './routes/BookRoute'
 import MemberRoute from './routes/MemberRoute'
 import BorrowRoute from './routes/BorrowRoute'
 
+import swaggerjsdoc from 'swagger-jsdoc'
+import swaggerui from 'swagger-ui-express'
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -11,6 +14,32 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req: any, res: any) => {
   res.send('Hello Worldss!')
 })
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Eigen BE Test - API Documentation by MHAG",
+      version: "1.0",
+      description: "API Documentation for Eigen BE Test",
+      contact: {
+        name: "Gip",
+        url: "https://ghifaryh.github.io/portfolio",
+        email: "mhghifaryy@gmail.com",
+      }
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`
+      },
+    ]
+  },
+  apis: ['./routes/*.ts']
+}
+
+const spacs = swaggerjsdoc(options)
+
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`)
--- be-test-case/routes/BookRoute.ts
+++ be-test-case/routes/BookRoute.ts
@@ -1,13 +1,108 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Books:
+ *       type: object
+ *       required:
+ *         - code
+ *         - title
+ *         - author
+ *         - stock
+ *       properties:
+ *         code:
+ *           type: string
+ *           description: The unique identifier of the book
+ *         title:
+ *           type: string
+ *           description: The title of the book
+ *         author:
+ *           type: string
+ *           description: The book writer
+ *         stock:
+ *           type: integer
+ *           description: To check if the book is available
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books manager
+ * /books:
+ *   get:
+ *     summary: Lists all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Books'
+ * /books/{code}:
+ *   get:
+ *     summary: Get the book by code
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: code
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book code
+ *     responses:
+ *       200:
+ *         description: The book response by code
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *       404:
+ *         description: The book was not found
+ *   patch:
+ *    summary: Update the book stock by the book code
+ *    tags: [Books]
+ *    parameters:
+ *      - in: path
+ *        name: code
+ *        required: true
+ *        description: The book book code
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              stock:
+ *                type: integer
+ *                minimum: 0
+ *                description: Jumlah stok baru untuk buku yang akan diperbarui.
+ *    responses:
+ *      200:
+ *        description: The book is updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Books'
+ *      404:
+ *        description: The book is not found
+ *      500:
+ *        description: Some error happened
+ */
 import express from 'express'
 import { getBookByCode, getBooks, updateBook } from '../controller/BookController'
-// import bodyParser from 'body-parser'
 
 const router = express.Router()
 
 router.get('/books', getBooks)
 router.get('/books/:code', getBookByCode)
 
-// router.use(bodyParser.json());
 router.patch('/books/:code', updateBook)
 
 export default router
